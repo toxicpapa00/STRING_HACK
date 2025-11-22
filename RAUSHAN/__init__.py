ab8e830e522a30c225aad67f9e37d9b8c880a3bb
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-22 13:07:42 +0530

    Update __init__.py

--- RAUSHAN/__init__.py
+++ RAUSHAN/__init__.py
@@ -39,11 +39,11 @@ async def RAUSHAN():
     header = Table(show_header=True, header_style="bold green")
     header.add_column(LOG_TEXT)
     LOG.print(header)
-    LOG.print(f"[bold cyan]𝐓𝐄𝐀𝐌 𝐏𝐔𝐑𝐕𝐈 𝐁𝐎𝐓𝐒")
+    LOG.print(f"[bold cyan]𝐓𝐄𝐀𝐌 𝗞𝗜𝗧𝗧𝗨 𝐁𝐎𝐓𝐒")
     LOG.print("[bold yellow]𝐘𝐨𝐮𝐫 𝐁𝐨𝐭 𝐒𝐭𝐚𝐫𝐭.............")
     await app.start()    
     
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(RAUSHAN())    
+loop.run_until_complete(TOXIC())    
